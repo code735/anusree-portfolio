3300ef23fa0f8157ea92f108fc129e82d04cd70c
CODE735 <83807896+code735@users.noreply.github.com>
2022-03-10 16:38:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function slide_in() {
+  let slide = document.getElementById("menu-slide");
+  slide.style.right = "0px";
+}
+
+function slide_out() {
+  document.getElementById("menu-slide").style.right = "-50%";
+}
